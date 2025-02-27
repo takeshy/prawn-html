791ae965f7ce8f9fe5675d9d565d5bd50b1cb426
takeshi morita <takesy@joins.co.jp>
2025-02-27 13:45:31 +0900

    adjust image width to fit PDF

--- lib/prawn_html/tags/img.rb
+++ lib/prawn_html/tags/img.rb
@@ -15,9 +15,13 @@ module PrawnHtml
       def custom_render(pdf, context)
         parsed_styles = Attributes.parse_styles(attrs.style)
         block_styles = context.block_styles
-        evaluated_styles = adjust_styles(pdf, block_styles.merge(parsed_styles))
 
         src = @attrs.src
+
+        # Prepare image options
+        evaluated_styles = adjust_styles(pdf, block_styles.merge(parsed_styles))
+
+        # Render the image
         if remote_url?(src)
           begin
             # Download the image to a temporary file
@@ -25,7 +29,6 @@ module PrawnHtml
             pdf.image(temp_file.path, evaluated_styles)
           rescue StandardError => e
             # If download fails, try to use the src directly
-            # This will likely fail if the URL is not accessible, but it's worth a try
             pdf.image(src, evaluated_styles)
           end
         else
@@ -56,8 +59,29 @@ module PrawnHtml
       def adjust_styles(pdf, img_styles)
         {}.tap do |result|
           w, h = img_styles['width'], img_styles['height']
-          result[:width] = Utils.convert_size(w, options: pdf.page_width) if w
-          result[:height] = Utils.convert_size(h, options: pdf.page_height) if h
+
+          # Convert specified width and height to PDF units
+          width = w ? Utils.convert_size(w, options: pdf.page_width) : nil
+          height = h ? Utils.convert_size(h, options: pdf.page_height) : nil
+
+          # If no dimensions specified, use page dimensions
+          if !width && !height
+            width = pdf.page_width
+            height = pdf.page_height
+          end
+
+          # Ensure image fits within page dimensions
+          width = pdf.page_width if width && width > pdf.page_width
+          height = pdf.page_height if height && height > pdf.page_height
+
+          # Set the dimensions
+          result[:width] = width if width
+          result[:height] = height if height
+
+          # Set fit option to stretch the image to the specified dimensions
+          result[:fit] = [width, height] if width && height
+
+          # Set position if specified
           result[:position] = img_styles[:align] if %i[left center right].include?(img_styles[:align])
         end
       end
--- lib/prawn_html/version.rb
+++ lib/prawn_html/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module PrawnHtml # :nodoc:
-  VERSION = '0.7.1'
+  VERSION = '0.7.2'
 end
